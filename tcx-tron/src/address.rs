1bf64c417dd02d52280ba919bff2ff24ed2b7467
Neal Xu <imxuneal@gmail.com>
2019-09-06 15:17:15 +0800

    Fix tron-address test

--- tcx-tron/src/address.rs
+++ tcx-tron/src/address.rs
@@ -4,7 +4,7 @@ use tcx_chain::keystore::Address as TraitAddress;
 use tcx_chain::PublicKey;
 use tcx_primitive::key::secp256k1::Public;
 
-pub struct Address(String);
+pub struct Address(pub String);
 
 pub enum Error {
     InvalidBase58,
@@ -40,12 +40,17 @@ impl TraitAddress for Address {
 #[cfg(test)]
 mod tests {
     use super::Address;
+    use tcx_chain::keystore::Address as TraitAddress;
+    use tcx_chain::PublicKey;
+    use tcx_chain::Secp256k1PublicKey;
 
     #[test]
     fn tron_address() {
         let bytes = hex::decode("04DAAC763B1B3492720E404C53D323BAF29391996F7DD5FA27EF0D12F7D50D694700684A32AD97FF4C09BF9CF0B9D0AC7F0091D9C6CB8BE9BB6A1106DA557285D8").unwrap();
-        //let public_key = <bitcoin::PublicKey as PublicKey>::from_slice(&bytes).unwrap();
-
-        //        assert_eq!(Address::from_public_key(&public_key).unwrap().0, "THfuSDVRvSsjNDPFdGjMU19Ha4Kf7acotq");
+        let public_key = Secp256k1PublicKey::from_slice(&bytes).unwrap();
+        assert_eq!(
+            Address::from_public_key(&public_key, None).unwrap(),
+            "THfuSDVRvSsjNDPFdGjMU19Ha4Kf7acotq"
+        );
     }
 }
