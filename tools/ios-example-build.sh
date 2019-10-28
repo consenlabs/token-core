342c24bc8c42e18f6011eac62ff4487fccdadc07
Neal Xu <imxuneal@gmail.com>
2019-10-28 15:59:49 +0800

    Add the secp256k1 dep

--- tools/ios-example-build.sh
+++ tools/ios-example-build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-pushd ../tcx
-RUST_BACKTRACE=1 cbindgen src/lib.rs -l c > cheader/tcx.h
-cargo lipo --release
-
 if [ ! -d "../examples/iOSExample/TokenCoreX" ]; then
   mkdir -p ../examples/iOSExample/TokenCoreX/Include
   mkdir -p ../examples/iOSExample/TokenCoreX/Libs
 fi
+
+pushd ../libs/secp256k1
+cargo lipo --release
+
+cp target/universal/release/libsecp256k1.a ../../examples/iOSExample/TokenCoreX/Libs
+popd
+
+pushd ../tcx
+RUST_BACKTRACE=1 cbindgen src/lib.rs -l c > cheader/tcx.h
+cargo lipo --release
 
 cp cheader/tcx.h ../examples/iOSExample/TokenCoreX/Include
 cp ../target/universal/release/libtcx.a ../examples/iOSExample/TokenCoreX/Libs
