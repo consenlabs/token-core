4aa431fdad5d7825879631ed0f4e0271231687b4
Kai Chen <kai@thekaiway.com>
2019-11-25 10:30:29 +0800

    tool: tweak ios build script

--- tools/ios-example-build.sh
+++ tools/ios-example-build.sh
@@ -1,32 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-LIBS=../examples/iOSExample/TokenCoreX
-
+WORKDIR=$(pwd)
+LIBS=$WORKDIR/examples/iOSExample/TokenCoreX
 
 if [ ! -d $LIBS ]; then
-  mkdir -p $LIBS/Include
-  mkdir -p $LIBS/Libs
+  mkdir -p $LIBS/Include $LIBS/Libs
 fi
 
-pushd ../libs/secp256k1
+pushd ./libs/secp256k1
+git submodule update --init --recursive
 if ! type "cargo-lipo" > /dev/null; then
     cargo install cargo-lipo
     rustup target add aarch64-apple-ios x86_64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios
 fi
-LIBS=../../examples/iOSExample/TokenCoreX
 cargo lipo --release --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
-
 cp target/universal/release/libsecp256k1.a $LIBS/Libs
 popd
 
-pushd ../tcx
-LIBS=../examples/iOSExample/TokenCoreX
+pushd ./tcx
 if [ ! -d cheader ]; then
   mkdir -p cheader
 fi
 RUST_BACKTRACE=1 cbindgen src/lib.rs -l c > cheader/tcx.h
 cargo lipo --release  --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
-
 cp cheader/tcx.h $LIBS/Include
 cp ../target/universal/release/libtcx.a $LIBS/Libs
 popd
