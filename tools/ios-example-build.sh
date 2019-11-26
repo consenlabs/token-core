795abbd2c41a8727f1d6ace43c196bc4571d2322
xwartz <stddup@gmail.com>
2019-11-26 11:16:50 +0800

    build(RN): update build tools

--- tools/ios-example-build.sh
+++ tools/ios-example-build.sh
@@ -2,9 +2,14 @@
 
 WORKDIR=$(pwd)
 LIBS=$WORKDIR/examples/iOSExample/TokenCoreX
+RNLIBS=$WORKDIR/examples/RN/ios/TokenCoreX
 
 if [ ! -d $LIBS ]; then
   mkdir -p $LIBS/Include $LIBS/Libs
+fi
+
+if [ ! -d $RNLIBS ]; then
+  mkdir -p $RNLIBS/Include $RNLIBS/Libs
 fi
 
 pushd ./libs/secp256k1
@@ -15,6 +20,7 @@ if ! type "cargo-lipo" > /dev/null; then
 fi
 cargo lipo --release --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
 cp target/universal/release/libsecp256k1.a $LIBS/Libs
+cp target/universal/release/libsecp256k1.a $RNLIBS/Libs
 popd
 
 pushd ./tcx
@@ -24,5 +30,7 @@ fi
 RUST_BACKTRACE=1 cbindgen src/lib.rs -l c > cheader/tcx.h
 cargo lipo --release  --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
 cp cheader/tcx.h $LIBS/Include
+cp cheader/tcx.h $RNLIBS/Include
 cp ../target/universal/release/libtcx.a $LIBS/Libs
+cp ../target/universal/release/libtcx.a $RNLIBS/Libs
 popd
