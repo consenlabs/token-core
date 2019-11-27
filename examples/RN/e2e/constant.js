382d37adcef6a22209a61768f5ce97a9d409f81d
xwartz <stddup@gmail.com>
2019-11-27 15:08:57 +0800

    e2e: fix bch test

--- examples/RN/e2e/constant.js
+++ examples/RN/e2e/constant.js
@@ -3,7 +3,7 @@ export const SUPPORT_CHAINTYPES = ['BITCOINCASH', 'LITECOIN', 'TRON']
 
 export const MNEMONIC = 'inject kidney empty canal shadow pact comfort wife crush horse wife sketch'
 
-export const ADDRESS_BCH = 'bchtest:qqurlwqukz3lcujttcyvlzaagppnd4c37chrtrylmc'
+export const ADDRESS_BCH = 'qqurlwqukz3lcujttcyvlzaagppnd4c37chrtrylmc'
 export const ADDRESS_LTC = 'mkeNU5nVnozJiaACDELLCsVUc8Wxoh1rQN'
 export const ADDRESS_TRON = 'TY2uroBeZ5trA9QT96aEWj32XLkAAhQ9R2'
 
