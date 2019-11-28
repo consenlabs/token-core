e29b799bb6658ec0c012040f4610d4b3967b5546
Neal Xu <imxuneal@gmail.com>
2019-11-28 16:06:21 +0800

    use x86_64 target only

--- tools/ios-example-build.sh
+++ tools/ios-example-build.sh
@@ -16,9 +16,9 @@ pushd ./libs/secp256k1
 git submodule update --init --recursive
 if ! type "cargo-lipo" > /dev/null; then
     cargo install cargo-lipo
-    rustup target add aarch64-apple-ios x86_64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios
+    rustup target add x86_64-apple-ios
 fi
-cargo lipo --release --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
+cargo lipo --release --targets x86_64-apple-ios
 cp target/universal/release/libsecp256k1.a $LIBS/Libs
 cp target/universal/release/libsecp256k1.a $RNLIBS/Libs
 popd
@@ -28,7 +28,7 @@ if [ ! -d cheader ]; then
   mkdir -p cheader
 fi
 RUST_BACKTRACE=1 cbindgen src/lib.rs -l c > cheader/tcx.h
-cargo lipo --release  --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
+cargo lipo --release  --targets x86_64-apple-ios
 cp cheader/tcx.h $LIBS/Include
 cp cheader/tcx.h $RNLIBS/Include
 cp ../target/universal/release/libtcx.a $LIBS/Libs
