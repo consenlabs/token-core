28687e6b6dd55070013ad59208c8f95fbaf3b2a2
Neal Xu <imxuneal@gmail.com>
2019-11-29 11:12:32 +0800

    Add the tcx-proto

--- tcx-proto/build.rs
+++ tcx-proto/build.rs
@@ -2,8 +2,17 @@ use std::env;
 extern crate prost_build;
 
 fn main() {
-    //    env::set_var("OUT_DIR", "src");
-    //    prost_build::compile_protos(&["src/api.proto"], &["src/"]).unwrap();
+    // tcx-api
+    env::set_var("OUT_DIR", "../tcx/src");
+    prost_build::compile_protos(
+        &[
+            "src/api.proto",
+            "src/api_params.proto",
+            "src/cache_derived_key.proto",
+        ],
+        &["src/"],
+    )
+    .unwrap();
 
     //    let targets = vec!["arm64-v8a", "armeabi-v7a", "x86", "x86_64"];
     //    for target in targets {
--- tcx/src/api.rs
+++ tcx/src/api.rs
@@ -15,28 +15,6 @@ pub struct InitTokenCoreXParam {
     pub xpub_common_iv: std::string::String,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct InitTokenCoreXParamCopy {
-    #[prost(string, tag = "1")]
-    pub file_dir1: std::string::String,
-    #[prost(string, tag = "2")]
-    pub xpub_common_key: std::string::String,
-    #[prost(string, tag = "3")]
-    pub xpub_common_iv: std::string::String,
-}
-//r#"{
-//"chainType":"BITCOINCASH",
-//"mnemonic":"inject kidney empty canal shadow pact comfort wife crush horse wife sketch",
-//"name":"BCH-Wallet-1",
-//"network":"MAINNET",
-//"overwrite":true,
-//"password":"Insecure Password",
-//"passwordHint":"",
-//"path":"m/44'/145'/0'/0/0",
-//"segWit":"NONE",
-//"source":"MNEMONIC"
-//}"#;
-
-#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ImportWalletFromMnemonicParam {
     #[prost(string, tag = "1")]
     pub chain_type: std::string::String,
@@ -59,22 +37,6 @@ pub struct ImportWalletFromMnemonicParam {
     #[prost(bool, tag = "10")]
     pub overwrite: bool,
 }
-//{
-//"address": "mkeNU5nVnozJiaACDELLCsVUc8Wxoh1rQN",
-//"chainType": "LITECOIN",
-//"createdAt": 1566455834,
-//"encXPub": "GekyMLycBJlFAmob0yEGM8zrEKrBHozAKr66PrMts7k6vSBJ/8DJQW7HViVqWftKhRbPAxZ3MO0281AKvWp4qa+/Q5nqoCi5/THxRLA1wDn8gWqDJjUjaZ7kJaNnreWfUyNGUeDxnN7tHDGdW4nbtA==",
-//"externalAddress": {
-//"address": "mj78AbVtQ9SWnvbU7pcrueyE1krMmZtoUU",
-//"derivedPath": "0/1",
-//"type": "EXTERNAL"
-//},
-//"id": "fdb5e9d4-530d-46ed-bf4a-6a27fb8eddca",
-//"name": "LTC-Wallet-1",
-//"passwordHint": "",
-//"source": "MNEMONIC"
-//}
-
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct WalletResult {
     #[prost(string, tag = "1")]
@@ -109,4 +71,34 @@ pub mod external_address_extra {
         #[prost(string, tag = "3")]
         pub r#type: std::string::String,
     }
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct WalletKey {
+    #[prost(string, tag = "1")]
+    pub id: std::string::String,
+    #[prost(string, tag = "2")]
+    pub password: std::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CacheDerivedKeyParam {
+    #[prost(string, tag = "1")]
+    pub id: std::string::String,
+    #[prost(string, tag = "2")]
+    pub derived_key: std::string::String,
+    #[prost(string, tag = "3")]
+    pub temp_password: std::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct VerifyDerivedKeyParam {
+    #[prost(string, tag = "1")]
+    pub id: std::string::String,
+    #[prost(string, tag = "2")]
+    pub derived_key: std::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct DerivedKeyResult {
+    #[prost(string, tag = "1")]
+    pub id: std::string::String,
+    #[prost(string, tag = "2")]
+    pub derived_key: std::string::String,
 }
