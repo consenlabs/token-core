a102259b98d6c5039d6b70a468e6ed78aee2de97
zys20080808 <zys20080808@126.com>
2019-12-10 14:45:03 +0800

    Update E2E constant.js

--- examples/RN/e2e/constant.js
+++ examples/RN/e2e/constant.js
@@ -1,25 +1,32 @@
 export const PASSWORD = '123123123!@#$%'
 
 export const MNEMONIC_12 = 'inject kidney empty canal shadow pact comfort wife crush horse wife sketch'
-export const MNEMONIC_24 = 'install mass display evidence cave prize echo soda clever beef orphan canyon hint area jar you crazy salad cradle balance solve club yellow advice'
-
-// TESTNET 12 MNEMONIC ADDRESS
+// TESTNET MNEMONIC_12 ADDRESS
 export const BITCOINCASH_TESTNET_MNEMONIC_12_ADDRESS = 'qqurlwqukz3lcujttcyvlzaagppnd4c37chrtrylmc'
 export const LITECOIN_TESTNET_MNEMONIC_12_ADDRESS = 'mkeNU5nVnozJiaACDELLCsVUc8Wxoh1rQN'
 export const TRON_TESTNET_MNEMONIC_12_ADDRESS = 'TY2uroBeZ5trA9QT96aEWj32XLkAAhQ9R2'
-// TESTNET 24 MNEMONIC ADDRESS
-export const BITCOINCASH_TESTNET_MNEMONIC_24_ADDRESS = 'qr9lsgw69856d4ptnl9wp08emrv53kx8gvfqvrf6kj'
-export const LITECOIN_TESTNET_MNEMONIC_24_ADDRESS = 'mz7SiXSBFfaBPLhhRWdFMNCcvdPxSWo9vT'
-export const TRON_TESTNET_MNEMONIC_24_ADDRESS = 'TPULP1o2CfsyLupsQABm4hHTUsWXiYpmBA'
- 
-// MAINNET 12 MNEMONIC ADDRESS
+
+// MAINNET MNEMONIC_12 ADDRESS
 export const BITCOINCASH_MAINNET_MNEMONIC_12_ADDRESS = 'qzld7dav7d2sfjdl6x9snkvf6raj8lfxjcj5fa8y2r'
 export const LITECOIN_MAINNET_MNEMONIC_12_ADDRESS = 'Ldfdegx3hJygDuFDUA7Rkzjjx8gfFhP9DP'
 export const TRON_MAINNET_MNEMONIC_12_ADDRESS = 'TY2uroBeZ5trA9QT96aEWj32XLkAAhQ9R2'
-// MAINNET 24 MNEMONIC ADDRESS
+
+// MAINNET MNEMONIC_12 PRIVATEKEY
+export const BITCOINCASH_MAINNET_MNEMONIC_12_PRIVATEKEY = 'L39VXyorp19JfsEJfbD7Tfr4pBEX93RJuVXW7E13C51ZYAhUWbYa'
+export const LITECOIN_MAINNET_MNEMONIC_12_PRIVATEKEY = 'T4oDFhweKSqfPjiebJb6hoCLJKNFsWTYHfodfGyuoGy5yHoete7k'
+export const TRON_MAINNET_MNEMONIC_12_PRIVATEKEY = ''
+
+export const MNEMONIC_24 = 'install mass display evidence cave prize echo soda clever beef orphan canyon hint area jar you crazy salad cradle balance solve club yellow advice'
+// TESTNET MNEMONIC_24 ADDRESS
+export const BITCOINCASH_TESTNET_MNEMONIC_24_ADDRESS = 'qr9lsgw69856d4ptnl9wp08emrv53kx8gvfqvrf6kj'
+export const LITECOIN_TESTNET_MNEMONIC_24_ADDRESS = 'mz7SiXSBFfaBPLhhRWdFMNCcvdPxSWo9vT'
+export const TRON_TESTNET_MNEMONIC_24_ADDRESS = 'TPULP1o2CfsyLupsQABm4hHTUsWXiYpmBA'
+// MAINNET MNEMONIC_24 ADDRESS
 export const BITCOINCASH_MAINNET_MNEMONIC_24_ADDRESS = 'qzrg7hg66dwxupa48je67ca2m08wd5s9t53l3e5l7n'
 export const LITECOIN_MAINNET_MNEMONIC_24_ADDRESS = 'LWPTmrBhMfezodS721xizSvdmHfRWqWnEe'
 export const TRON_MAINNET_MNEMONIC_24_ADDRESS = 'TPULP1o2CfsyLupsQABm4hHTUsWXiYpmBA'
+
+
 
 // MAINNET PRIVATEKEY
 export const BITCOINCASH_MAINNET_PRIVATEKEY = 'L1xWKe22XgDXzGgZxvUNtXqqQyWzY5bYp2vt7qBWYzdBNxwhXguN'
