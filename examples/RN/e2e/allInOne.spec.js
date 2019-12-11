8c9bd63765d499c00412f9ee703e3946c67b5940
Neal Xu <imxuneal@gmail.com>
2019-12-11 20:20:34 +0800

    test signtx error

--- examples/RN/e2e/allInOne.spec.js
+++ examples/RN/e2e/allInOne.spec.js
@@ -4,9 +4,9 @@ import createFlow from './specs/wallet/createFlow'
 import signTxFlow from './specs/wallet/signTxFlow'
 import tronSignMessageFlow from './specs/wallet/tronSignMessageFlow'
 
-createFlow()
-mnemonicFlow()
-privateKeyFlow()
+// createFlow()
+// mnemonicFlow()
+// privateKeyFlow()
 signTxFlow()
-tronSignMessageFlow()
+// tronSignMessageFlow()
 
