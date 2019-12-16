c2a2a4f8e5c9bf3617cfc3783ebef38e372d5823
tyrone <2125081@qq.com>
2019-12-16 20:24:26 +0800

    Change all of ckb bytes type to string
    Signed-off-by: tyrone <2125081@qq.com>

--- tcx-ckb/src/lib.rs
+++ tcx-ckb/src/lib.rs
@@ -9,6 +9,7 @@ use failure::Fail;
 
 pub use address::CkbAddress;
 pub use serializer::Serializer;
+use tcx_chain::Result;
 pub use transaction::{CachedCell, CellInput, CkbTxInput, CkbTxOutput, OutPoint, Script, Witness};
 
 #[derive(Fail, Debug, PartialEq)]
@@ -39,4 +40,31 @@ pub enum Error {
 
     #[fail(display = "cell_input_not_cached")]
     CellInputNotCached,
+
+    #[fail(display = "invalid_hex_value")]
+    InvalidHexValue,
+}
+
+pub fn hex_to_bytes(value: &str) -> Result<Vec<u8>> {
+    let result = if value.starts_with("0x") || value.starts_with("0X") {
+        hex::decode(&value[2..])
+    } else {
+        hex::decode(&value[..])
+    };
+
+    result.map_err(|_| Error::InvalidHexValue.into())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::hex_to_bytes;
+
+    #[test]
+    pub fn hex_convert() {
+        let v: Vec<u8> = vec![];
+        assert_eq!(v, hex_to_bytes("0x").unwrap());
+        assert_eq!(vec![0x01], hex_to_bytes("0x01").unwrap());
+        assert_eq!(vec![0x02], hex_to_bytes("0x02").unwrap());
+        assert_eq!(vec![0x02, 0x11], hex_to_bytes("0x0211").unwrap());
+    }
 }
--- tcx-ckb/src/transaction_helper.rs
+++ tcx-ckb/src/transaction_helper.rs
@@ -3,6 +3,7 @@ use crate::transaction::{Script, Witness};
 
 use super::Error;
 use crate::hash::blake2b_256;
+use crate::hex_to_bytes;
 use tcx_chain::Result;
 
 impl Script {
@@ -16,9 +17,9 @@ impl Script {
 
     pub fn serialize(&self) -> Result<Vec<u8>> {
         Ok(Serializer::serialize_dynamic_vec(&vec![
-            self.code_hash.clone(),
+            hex_to_bytes(&self.code_hash)?.clone(),
             self.serialize_hash_type()?,
-            Serializer::serialize_fixed_vec(&vec![self.args.clone()]),
+            Serializer::serialize_fixed_vec(&vec![hex_to_bytes(&self.args)?.clone()]),
         ]))
     }
 
@@ -28,20 +29,21 @@ impl Script {
 }
 
 impl Witness {
-    pub fn serialize(&self) -> Vec<u8> {
-        let inner_serialize = |x: &Vec<u8>| {
-            if x.len() > 0 {
-                Serializer::serialize_fixed_vec(&vec![x.clone()])
+    pub fn serialize(&self) -> Result<Vec<u8>> {
+        let inner_serialize = |x: &str| -> Result<Vec<u8>> {
+            let bytes = hex_to_bytes(&x)?;
+            if bytes.len() > 0 {
+                Ok(Serializer::serialize_fixed_vec(&vec![bytes]))
             } else {
-                vec![]
+                Ok(vec![])
             }
         };
 
-        Serializer::serialize_dynamic_vec(&vec![
-            inner_serialize(&self.lock),
-            inner_serialize(&self.input_type),
-            inner_serialize(&self.output_type),
-        ])
+        Ok(Serializer::serialize_dynamic_vec(&vec![
+            inner_serialize(&self.lock)?,
+            inner_serialize(&self.input_type)?,
+            inner_serialize(&self.output_type)?,
+        ]))
     }
 }
 
@@ -52,22 +54,18 @@ mod tests {
     #[test]
     fn serialize_script() {
         let script = Script {
-            code_hash: hex::decode(
-                "68d5438ac952d2f584abf879527946a537e82c7f3c1cbf6d8ebf9767437d8e88",
-            )
-            .unwrap(),
-            args: hex::decode("3954acece65096bfa81258983ddb83915fc56bd8").unwrap(),
+            code_hash: "0x68d5438ac952d2f584abf879527946a537e82c7f3c1cbf6d8ebf9767437d8e88"
+                .to_owned(),
+            args: "0x3954acece65096bfa81258983ddb83915fc56bd8".to_owned(),
             hash_type: "type".to_string(),
         };
 
         assert_eq!(script.serialize().unwrap(), hex::decode("4900000010000000300000003100000068d5438ac952d2f584abf879527946a537e82c7f3c1cbf6d8ebf9767437d8e8801140000003954acece65096bfa81258983ddb83915fc56bd8").unwrap());
 
         let script = Script {
-            code_hash: hex::decode(
-                "68d5438ac952d2f584abf879527946a537e82c7f3c1cbf6d8ebf9767437d8e88",
-            )
-            .unwrap(),
-            args: vec![],
+            code_hash: "0x68d5438ac952d2f584abf879527946a537e82c7f3c1cbf6d8ebf9767437d8e88"
+                .to_owned(),
+            args: "0x".to_owned(),
             hash_type: "type".to_string(),
         };
 
@@ -77,70 +75,61 @@ mod tests {
     #[test]
     fn script_to_hash() {
         let script = Script {
-            code_hash: hex::decode(
-                "0000000000000000000000000000000000000000000000000000000000000000",
-            )
-            .unwrap(),
-            args: vec![],
+            code_hash: "0x0000000000000000000000000000000000000000000000000000000000000000"
+                .to_owned(),
+            args: "0x".to_owned(),
             hash_type: "data".to_string(),
         };
 
         assert_eq!(
-            script.to_hash().unwrap(),
-            hex::decode("77c93b0632b5b6c3ef922c5b7cea208fb0a7c427a13d50e13d3fefad17e0c590")
-                .unwrap()
+            hex::encode(script.to_hash().unwrap()),
+            "77c93b0632b5b6c3ef922c5b7cea208fb0a7c427a13d50e13d3fefad17e0c590"
         );
 
         let script = Script {
-            code_hash: hex::decode(
-                "0000000000000000000000000000000000000000000000000000000000000000",
-            )
-            .unwrap(),
-            args: vec![0x1],
+            code_hash: "0x0000000000000000000000000000000000000000000000000000000000000000"
+                .to_owned(),
+            args: "0x01".to_owned(),
             hash_type: "data".to_string(),
         };
         assert_eq!(
-            script.to_hash().unwrap(),
-            hex::decode("67951b34bce20cb71b7e235c1f8cda259628d99d94825bffe549c23b4dd2930f")
-                .unwrap()
+            hex::encode(script.to_hash().unwrap()),
+            "67951b34bce20cb71b7e235c1f8cda259628d99d94825bffe549c23b4dd2930f"
         );
 
         let script = Script {
-            code_hash: hex::decode(
-                "0000000000000000000000000000000000000000000000000000000000000000",
-            )
-            .unwrap(),
-            args: vec![0x1],
+            code_hash: "0x0000000000000000000000000000000000000000000000000000000000000000"
+                .to_owned(),
+            args: "0x01".to_owned(),
             hash_type: "type".to_string(),
         };
 
         assert_eq!(
-            script.to_hash().unwrap(),
-            hex::decode("d39f84d4702f53cf8625da4411be1640b961715cb36816501798fedb70b6e0fb")
-                .unwrap()
+            hex::encode(script.to_hash().unwrap()),
+            "d39f84d4702f53cf8625da4411be1640b961715cb36816501798fedb70b6e0fb",
         );
     }
 
     #[test]
     fn serialize_witness() {
         let witness = Witness {
-            lock: vec![],
-            input_type: vec![],
-            output_type: vec![],
+            lock: "0x".to_owned(),
+            input_type: "0x".to_owned(),
+            output_type: "0x".to_owned(),
         };
 
         assert_eq!(
-            hex::encode(witness.serialize()),
+            hex::encode(witness.serialize().unwrap()),
             "10000000100000001000000010000000"
         );
 
         let witness = Witness {
-            lock: vec![],
-            input_type: vec![0x10],
-            output_type: vec![0x20],
+            lock: "0x".to_owned(),
+            input_type: "0x10".to_owned(),
+            output_type: "0x20".to_owned(),
         };
         assert_eq!(
-            hex::encode(witness.serialize()),
+            hex::encode(witness.serialize().unwrap()),
             "1a00000010000000100000001500000001000000100100000020"
         );
     }
