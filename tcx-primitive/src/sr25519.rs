d2be105de3cc606137dfc98976ff30648126f28f
Neal Xu <imxuneal@gmail.com>
2020-04-27 14:29:04 +0800

    Remove all unused code

--- tcx-primitive/src/sr25519.rs
+++ tcx-primitive/src/sr25519.rs
@@ -42,18 +42,9 @@ impl TraitPrivateKey for Sr25519PrivateKey {
 
     fn sign(&self, data: &[u8]) -> Result<Vec<u8>> {
         Ok(self.0.sign(data).0.to_vec())
-        //        let msg = Message::from_slice(data).map_err(transform_secp256k1_error)?;
-        //        let signature = SECP256K1_ENGINE.sign(&msg, &self.0.key);
-        //        Ok(signature.serialize_der().to_vec())
     }
 
     fn sign_recoverable(&self, data: &[u8]) -> Result<Vec<u8>> {
-        //
-        //        let msg = Message::from_slice(data).map_err(transform_secp256k1_error)?;
-        //        let signature = SECP256K1_ENGINE.sign_recoverable(&msg, &self.0.key);
-        //        let (recover_id, sign) = signature.serialize_compact();
-        //        let signed_bytes = [sign[..].to_vec(), vec![(recover_id.to_i32()) as u8]].concat();
-        //        Ok(signed_bytes)
         // https://www.deadalnix.me/2017/02/17/schnorr-signatures-for-not-so-dummies/
         self.sign(data)
     }
@@ -92,54 +83,3 @@ impl FromHex for Sr25519PublicKey {
         Ok(pk)
     }
 }
-//
-//impl Ss58Codec for Sr25519PublicKey {
-//    fn from_ss58check_with_version(s: &str) -> Result<(Self, Vec<u8>)> {
-//        // todo: address from
-//        let (addr, ver) = Public::from_ss58check_with_version(s)
-//            .map_err(|_| format_err!("parse address error"))?;
-//        Ok((Sr25519PublicKey::from(addr), vec!(ver)))
-//    }
-//
-//    fn to_ss58check_with_version(&self, version: &[u8]) -> String {
-////        sp_core::crypto::Ss58AddressFormat::frx;
-//        // todo: unwrap()
-//        let ver = Ss58AddressFormat::Custom(version.first().unwrap().clone());
-//        self.0.to_ss58check_with_version(ver)
-//    }
-//}
-
-//impl Ss58Codec for Sr25519PrivateKey {
-//    fn from_ss58check_with_version(wif: &str) -> Result<(Self, Vec<u8>)> {
-////        let data = base58::from_check(wif)?;
-////
-////        let compressed = match data.len() {
-////            33 => false,
-////            34 => true,
-////            _ => {
-////                return Err(KeyError::InvalidPrivateKey.into());
-////            }
-////        };
-////
-////        let pk = Secp256k1PrivateKey(PrivateKey {
-////            key: secp256k1::SecretKey::from_slice(&data[1..33])?,
-////            compressed,
-////            network: Network::Bitcoin,
-////        });
-//
-//        Ok((pk, vec![data[0]]))
-//    }
-//
-//    fn to_ss58check_with_version(&self, version: &[u8]) -> String {
-//        let mut ret = [0; 34];
-//        ret[0..1].copy_from_slice(&version[0..]);
-//        ret[1..33].copy_from_slice(&self.0.key[..]);
-//        if self.0.compressed {
-//            ret[33] = 1;
-//            base58::check_encode_slice(&ret[..]).to_string()
-//        } else {
-//            base58::check_encode_slice(&ret[..33]).to_string()
-//        }
-//    }
-//}
-//
