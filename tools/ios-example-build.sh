c9c559bc4df7564fc2aee5996106eeaccbd74df3
Neal Xu <imxuneal@gmail.com>
2020-07-02 14:11:49 +0800

    Remove the armv7

--- tools/ios-example-build.sh
+++ tools/ios-example-build.sh
@@ -12,10 +12,10 @@ git submodule update --init --recursive
 if ! type "cargo-lipo" > /dev/null; then
     cargo install cargo-lipo
     # rustup target add aarch64-apple-ios x86_64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios
-    rustup target add x86_64-apple-ios i386-apple-ios
+    rustup target add aarch64-apple-ios x86_64-apple-ios
 fi
 # cargo lipo --release --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
-cargo lipo --release --targets x86_64-apple-ios,i386-apple-ios
+cargo lipo --release --targets x86_64-apple-ios,aarch64-apple-ios
 cp target/universal/release/libsecp256k1.a $LIBS/Libs
 popd
 
@@ -24,7 +24,7 @@ if [ ! -d cheader ]; then
   mkdir -p cheader
 fi
 RUST_BACKTRACE=1 cbindgen src/lib.rs -l c > cheader/tcx.h
-cargo lipo --release  --targets x86_64-apple-ios
+cargo lipo --release  --targets x86_64-apple-ios,aarch64-apple-ios
 # cargo lipo --release  --targets aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,x86_64-apple-ios,i386-apple-ios
 cp cheader/tcx.h $LIBS/Include
 cp ../target/universal/release/libtcx.a $LIBS/Libs
--- tools/ios-framework-build.sh
+++ tools/ios-framework-build.sh
@@ -12,10 +12,10 @@ LIBS=examples/TokenCoreX/TokenCoreX
 pushd libs/secp256k1
 if ! type "cargo-lipo" > /dev/null; then
    cargo install cargo-lipo
-   rustup target add aarch64-apple-ios x86_64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios
+   rustup target add aarch64-apple-ios x86_64-apple-ios
 fi
 LIBS=../../examples/TokenCoreX/TokenCoreX
-cargo lipo --release --targets aarch64-apple-ios x86_64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios
+cargo lipo --release --targets aarch64-apple-ios x86_64-apple-ios
 
 cp target/universal/release/libsecp256k1.a $LIBS
 popd
@@ -26,7 +26,7 @@ if [ ! -d cheader ]; then
   mkdir -p cheader
 fi
 RUST_BACKTRACE=1 cbindgen src/lib.rs -l c > cheader/tcx.h
-cargo lipo --release  --targets aarch64-apple-ios x86_64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios
+cargo lipo --release  --targets aarch64-apple-ios x86_64-apple-ios
 
 cp cheader/tcx.h $LIBS
 cp ../target/universal/release/libtcx.a $LIBS
