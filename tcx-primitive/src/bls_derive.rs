1d80d39a92f50fb405261092dad493023598f05f
tyrone <2125081@qq.com>
2020-09-16 08:09:04 +0800

    Follow the latest EIP-2333
    Signed-off-by: tyrone <2125081@qq.com>

--- tcx-primitive/src/bls_derive.rs
+++ tcx-primitive/src/bls_derive.rs
@@ -1,11 +1,14 @@
+use bip39::{Language, Mnemonic};
+use num_bigint::BigUint;
+
 use super::Result;
+use crate::bls::{BLSPrivateKey, BLSPublicKey};
 use crate::ecc::KeyError;
 use crate::{Derive, DeterministicPrivateKey, DeterministicPublicKey, FromHex, PrivateKey, ToHex};
-
-use crate::bls::{BLSPrivateKey, BLSPublicKey};
-use bip39::{Language, Mnemonic};
-use bls_key_derivation::{derive_child, derive_master_sk};
-use num_bigint::BigUint;
+use crypto::digest::Digest;
+use crypto::hkdf::{hkdf_expand, hkdf_extract};
+use crypto::sha2::Sha256;
+use num_traits::{FromPrimitive, Num, Pow};
 
 #[derive(Clone)]
 pub struct BLSDeterministicPrivateKey(pub BigUint);
@@ -88,10 +91,155 @@ impl DeterministicPublicKey for BLSDeterministicPublicKey {
     }
 }
 
+// copy from https://github.com/ChainSafe/rust-bls-derivation/blob/master/src/key_derivation.rs
+// and follow the latest EIP-2333
+const DIGEST_SIZE: usize = 32;
+const NUM_DIGESTS: usize = 255;
+const OUTPUT_SIZE: usize = DIGEST_SIZE * NUM_DIGESTS;
+
+fn hkdf(salt: &[u8], ikm: &[u8], info: &[u8], okm: &mut [u8]) {
+    let digest = Sha256::new();
+    let prk: &mut [u8] = &mut [0u8; DIGEST_SIZE];
+    hkdf_extract(digest, salt, ikm, prk);
+    hkdf_expand(digest, prk, info, okm);
+}
+
+fn flip_bits(num: BigUint) -> BigUint {
+    num ^ (Pow::pow(
+        &BigUint::from_u64(2).unwrap(),
+        &BigUint::from_u64(256).unwrap(),
+    ) - &BigUint::from_u64(1).unwrap())
+}
+
+fn ikm_to_lamport_sk(ikm: &[u8], salt: &[u8], split_bytes: &mut [[u8; DIGEST_SIZE]; NUM_DIGESTS]) {
+    let mut okm = [0u8; OUTPUT_SIZE];
+    hkdf(salt, ikm, b"", &mut okm);
+    for r in 0..NUM_DIGESTS {
+        split_bytes[r].copy_from_slice(&okm[r * DIGEST_SIZE..(r + 1) * DIGEST_SIZE])
+    }
+}
+
+fn parent_sk_to_lamport_pk(parent_sk: BigUint, index: BigUint) -> Vec<u8> {
+    let salt = index.to_bytes_be();
+    let ikm = parent_sk.to_bytes_be();
+    let mut lamport_0 = [[0u8; DIGEST_SIZE]; NUM_DIGESTS];
+    ikm_to_lamport_sk(ikm.as_slice(), salt.as_slice(), &mut lamport_0);
+
+    let not_ikm = flip_bits(parent_sk).to_bytes_be();
+    let mut lamport_1 = [[0u8; DIGEST_SIZE]; NUM_DIGESTS];
+    ikm_to_lamport_sk(not_ikm.as_slice(), salt.as_slice(), &mut lamport_1);
+
+    let mut combined = [[0u8; DIGEST_SIZE]; NUM_DIGESTS * 2];
+    combined[..NUM_DIGESTS].clone_from_slice(&lamport_0[..NUM_DIGESTS]);
+    combined[NUM_DIGESTS..NUM_DIGESTS * 2].clone_from_slice(&lamport_1[..NUM_DIGESTS]);
+
+    let mut sha256 = Sha256::new();
+    let mut flattened_key = [0u8; OUTPUT_SIZE * 2];
+    for i in 0..NUM_DIGESTS * 2 {
+        let sha_slice = &mut combined[i];
+        sha256.input(sha_slice);
+        sha256.result(sha_slice);
+        sha256.reset();
+        flattened_key[i * DIGEST_SIZE..(i + 1) * DIGEST_SIZE].clone_from_slice(sha_slice);
+    }
+
+    sha256.input(&flattened_key);
+    let cmp_pk: &mut [u8] = &mut [0u8; DIGEST_SIZE];
+    sha256.result(cmp_pk);
+    cmp_pk.to_vec()
+}
+
+fn hkdf_mod_r(ikm: &[u8]) -> BigUint {
+    let mut okm = [0u8; 48];
+
+    let mut tmp = ikm.to_vec();
+    tmp.extend(b"\x00");
+
+    hkdf(b"BLS-SIG-KEYGEN-SALT-", &tmp, b"\x00\x30", &mut okm); // L=48, info=I2OSP(L,2)
+    let r = BigUint::from_str_radix(
+        "73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001",
+        16,
+    )
+    .unwrap();
+
+    BigUint::from_bytes_be(okm.as_ref()) % r
+}
+
+pub fn derive_child(parent_sk: BigUint, index: BigUint) -> BigUint {
+    let lamp_pk = parent_sk_to_lamport_pk(parent_sk, index);
+    hkdf_mod_r(lamp_pk.as_ref())
+}
+
+pub fn derive_master_sk(seed: &[u8]) -> Result<BigUint> {
+    if seed.len() < 16 {
+        return Err(failure::err_msg(
+            "seed must be greater than or equal to 16 bytes",
+        ));
+    }
+
+    Ok(hkdf_mod_r(seed))
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::bls_derive::BLSDeterministicPrivateKey;
     use crate::{Derive, DeterministicPrivateKey, PrivateKey};
+    use hex;
+    use num_bigint::BigUint;
+    use num_traits::{FromPrimitive, Num};
+
+    struct TestVector {
+        seed: &'static str,
+        master_sk: &'static str,
+        child_index: &'static str,
+        child_sk: &'static str,
+    }
+
+    #[test]
+    fn test_2333() {
+        let test_vectors = vec!(
+            TestVector{
+                seed : "c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04",
+                master_sk : "5399117110774477986698372024995405256382522670366369834617409486544348441851",
+                child_index : "0",
+                child_sk : "11812940737387919040225825939013910852517748782307378293770044673328955938106",
+            },
+            TestVector{
+                seed: "3141592653589793238462643383279502884197169399375105820974944592",
+                master_sk: "36167147331491996618072159372207345412841461318189449162487002442599770291484",
+                child_index: "3141592653",
+                child_sk: "41787458189896526028601807066547832426569899195138584349427756863968330588237",
+            },
+            TestVector{
+                seed: "0099FF991111002299DD7744EE3355BBDD8844115566CC55663355668888CC00",
+                master_sk: "13904094584487173309420026178174172335998687531503061311232927109397516192843",
+                child_index: "4294967295",
+                child_sk: "12482522899285304316694838079579801944734479969002030150864436005368716366140",
+            }
+        );
+
+        for t in test_vectors.iter() {
+            let seed = hex::decode(t.seed).expect("invalid seed format");
+            let master_sk = t
+                .master_sk
+                .parse::<BigUint>()
+                .expect("invalid master key format");
+            let child_index = t
+                .child_index
+                .parse::<BigUint>()
+                .expect("invalid index format");
+            let child_sk = t
+                .child_sk
+                .parse::<BigUint>()
+                .expect("invalid child key format");
+
+            let derived_master_sk = derive_master_sk(seed.as_ref()).unwrap();
+            assert_eq!(derived_master_sk, master_sk);
+            let pk = derive_child(master_sk, child_index);
+            assert_eq!(child_sk, pk);
+        }
+    }
 
     #[test]
     fn test_bls_derive() {
@@ -100,12 +248,12 @@ mod tests {
 
         assert_eq!(
             hex::encode(dsk.private_key().to_bytes()),
-            "41aaef9bc393b2aa2c9e082d1a75bef4c6428e7be7e35606485538aeba85aa1b"
+            "fbec74a665b4f52d36a1717c83b21e62051cd5cd90f1c81c4664a6f4bfcaef0b"
         );
 
         assert_eq!(
             hex::encode(dsk.derive("m/0").unwrap().private_key().to_bytes()),
-            "08c5678aa7be13f4a5693f3b230155307bf8cdf9c5c00d5654a21d3705506710"
+            "3a5542a9fef97a0f6b776fbe5e8edb0e087457be81223b1e1f40836834e31d1a"
         );
     }
 }
