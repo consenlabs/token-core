a66b88dd0e0af7a38ce1b9094b526945aed17bc9
Neal Xu <imxuneal@gmail.com>
2020-10-12 14:27:07 +0800

    fix: xcode 12 cannot combine framework

--- tools/ios-framework-build.sh
+++ tools/ios-framework-build.sh
@@ -56,15 +56,16 @@ mkdir -p $UNIVERSAL_OUTPUTFOLDER
 xcodebuild -target $PROJECT_NAME ONLY_ACTIVE_ARCH=NO -configuration $CONFIGURATION -sdk iphoneos BUILD_DIR=$BUILD_DIR BUILD_ROOT=$BUILD_ROOT build
 #export XCODE_XCCONFIG_FILE=/Users/xyz/Documents/code/token-core/examples/TokenCoreX/excluded_duplicated_arm64.xcconfig
 #xcodebuild -target $PROJECT_NAME -configuration $CONFIGURATION -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO BUILD_DIR=$BUILD_DIR BUILD_ROOT=$BUILD_ROOT -xcconfig /Users/xyz/Documents/code/token-core/examples/TokenCoreX/excluded_duplicated_arm64.xcconfig  build
+xcodebuild -target $PROJECT_NAME -configuration Debug -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO BUILD_DIR=$BUILD_DIR BUILD_ROOT=$BUILD_ROOT EXCLUDED_ARCHS=arm64 build
 
 # Step 2. Copy the framework structure (from iphoneos build) to the universal folder
 cp -R $BUILD_DIR/$CONFIGURATION-iphoneos/$PROJECT_NAME.framework $UNIVERSAL_OUTPUTFOLDER/
 # Step 3. Copy Swift modules from iphonesimulator build (if it exists) to the copied framework directory
 # BUILD_PRODUCTS=$SYMROOT/../../../../Products
-# cp -R $BUILD_PRODUCTS/Debug-iphonesimulator/$PROJECT_NAME.framework/Modules/$PROJECT_NAME.swiftmodule/. $UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework/Modules/$PROJECT_NAME.swiftmodule
+#cp -R $BUILD_PRODUCTS/Debug-iphonesimulator/$PROJECT_NAME.framework/Modules/$PROJECT_NAME.swiftmodule/. $UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework/Modules/$PROJECT_NAME.swiftmodule
 # Step 4. Create universal binary file using lipo and place the combined executable in the copied framework directory
-#lipo -create -output $UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework/$PROJECT_NAME $BUILD_PRODUCTS/Debug-iphonesimulator/$PROJECT_NAME.framework/$PROJECT_NAME $BUILD_DIR/$CONFIGURATION-iphoneos/$PROJECT_NAME.framework/$PROJECT_NAME
-lipo -create -output $UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework/$PROJECT_NAME $BUILD_DIR/$CONFIGURATION-iphoneos/$PROJECT_NAME.framework/$PROJECT_NAME
+lipo -create -output $UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework/$PROJECT_NAME $BUILD_PRODUCTS/Debug-iphonesimulator/$PROJECT_NAME.framework/$PROJECT_NAME $BUILD_DIR/$CONFIGURATION-iphoneos/$PROJECT_NAME.framework/$PROJECT_NAME
+#lipo -create -output $UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework/$PROJECT_NAME $BUILD_DIR/$CONFIGURATION-iphoneos/$PROJECT_NAME.framework/$PROJECT_NAME
 # Step 5. Convenience step to copy the framework to the project's directory
 cp -R $UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework ../../ios-release
 rm -rf $UNIVERSAL_OUTPUTFOLDER
